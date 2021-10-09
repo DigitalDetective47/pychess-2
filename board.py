c429fe341deaacf5698c7890912ae7aec8947c03
Ninji2701 <ninji2701@gmail.com>
2021-10-08 22:54:26 -0400

    Initial commit

---
+++ board.py
@@ -0,0 +1,210 @@
+from enum import Enum, Flag
+from enum import auto as enum_gen
+from typing import Any, Final, Optional, Sequence, SupportsIndex
+
+import pieces
+
+
+class Color(Enum):
+    "A player color."
+    NEUTRAL = enum_gen()
+    WHITE = enum_gen()
+    BLACK = enum_gen()
+
+    def next(self):
+        if self == Color.NEUTRAL:
+            raise ValueError("Color NEUTRAL has no next color")
+        return {Color.WHITE: Color.BLACK, Color.BLACK: Color.WHITE}[self]
+
+
+class CastlingRights(Flag):
+    NONE = 0
+    WHITE_KINGSIDE = enum_gen()
+    WHITE_QUEENSIDE = enum_gen()
+    BLACK_KINGSIDE = enum_gen()
+    BLACK_QUEENSIDE = enum_gen()
+
+
+class Coordinate:
+    "A location on a board."
+
+    def __init__(
+        self, position: str | Sequence[SupportsIndex], zero_index: Optional[bool] = None
+    ) -> None:
+        if isinstance(position, str):
+            if zero_index is not None:
+                raise TypeError(
+                    "argument zero_index should not be given if position is of type str"
+                )
+            elif not 2 <= len(position) < 4:
+                raise ValueError(
+                    "argument position must be of length 2 or 3 (not "
+                    + str(len(position))
+                    + ")"
+                )
+            elif not 97 <= ord(position[0]) < 123:
+                raise ValueError(
+                    "argument position must begin with a lowercase letter (not "
+                    + position[0]
+                    + ")"
+                )
+            elif not 1 <= int(position[1:]) < 27:
+                raise ValueError(
+                    "argument position must end with an integer between 1 and 26 (not "
+                    + position[1:]
+                    + ")"
+                )
+            pos_value: tuple[int, int] = (
+                ord(position[0]) - 97, int(position[1:]))
+        elif isinstance(position, Sequence):
+            if len(position) != 2:
+                raise ValueError(
+                    "argument position must be of length 2 (not "
+                    + str(len(position))
+                    + ")"
+                )
+            elif not isinstance(position[0], SupportsIndex):
+                raise TypeError(
+                    "file index must be of an index type (not "
+                    + type(position[0]).__name__
+                    + ")"
+                )
+            elif not isinstance(position[1], SupportsIndex):
+                raise TypeError(
+                    "row index must be of an index type (not "
+                    + type(position[0]).__name__
+                    + ")"
+                )
+            pos_value: tuple[int, int] = (
+                position[0].__index__() - (not zero_index),
+                position[1].__index__() - (not zero_index),
+            )
+            if not 0 <= self.pos[0] < 26:
+                raise IndexError(
+                    "file index must be between 0 and 25 (not " +
+                    str(self.pos[0]) + ")"
+                    if zero_index
+                    else "file index must be between 1 and 26 (not "
+                    + str(self.pos[0] + 1)
+                    + ")"
+                )
+            elif not 0 <= self.pos[1] < 26:
+                raise IndexError(
+                    "rank index must be between 0 and 25 (not " +
+                    str(self.pos[1]) + ")"
+                    if zero_index
+                    else "rank index must be between 1 and 26 (not "
+                    + str(self.pos[1] + 1)
+                    + ")"
+                )
+        else:
+            raise TypeError(
+                "argument position must be of type str or Sequence (not "
+                + type(position).__name__
+                + ")"
+            )
+        self.pos: Final[tuple[int, int]] = pos_value
+
+    def __add__(self, other):
+        if (
+            isinstance(other, Sequence)
+            and len(other) == 2
+            and isinstance(other[0], SupportsIndex)
+            and -26 <= other[0].__index__() < 27
+            and isinstance(other[1], SupportsIndex)
+            and -26 <= other[1].__index__() < 27
+        ):
+            coords: tuple[int, int] = (
+                self.pos[0] + other[0].__index__(),
+                self.pos[1] + other[1].__index__(),
+            )
+            if 0 <= coords[0] < 27 and 0 <= coords[1] < 27:
+                return Coordinate(coords, True)
+            raise IndexError("coordinate offset out of bounds")
+        return NotImplemented
+
+    def __bytes__(self) -> bytes:
+        return bytes(str(self))
+
+    def __eq__(self, other) -> bool:
+        return (
+            hash(self) == hash(other)
+            if isinstance(other, Coordinate)
+            else NotImplemented
+        )
+
+    def __hash__(self) -> int:
+        return self.pos[0] + (self.pos[1] << 5)
+
+    def __repr__(self) -> str:
+        return "Coordinate('" + str(self) + "')"
+
+    def __str__(self) -> str:
+        return chr(self.pos[0] + 97) + str(self.pos[1] + 1)
+
+
+class Board:
+    def __init__(
+        self,
+        fen: str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        **ex_attributes
+    ) -> None:
+        if not isinstance(fen, str):
+            raise TypeError(
+                "fen must be of type string (not " + type(fen).__name__ + ")"
+            )
+        fen_components: Final[list[str]] = fen.split(" ")
+        if len(fen_components) != 6:
+            raise ValueError("fen parameter is not valid fen")
+        rank_data: Final[list[str]] = fen_components[0].split("/")
+        self.ranks: Final[int] = len(rank_data) + 1
+        if self.ranks > 26:
+            raise ValueError("board cannot have more than 26 ranks")
+        num_files: int = -1
+        digit_buffer: str = ""
+        file: int
+        self.piece_array: dict[Coordinate, pieces.Piece] = {}
+        for rank in range(self.ranks):
+            file = 0
+            for char in rank_data[rank]:
+                if char.isdigit():
+                    digit_buffer += char
+                else:
+                    if digit_buffer:
+                        file += int(digit_buffer)
+                        digit_buffer = ""
+                    # Add a piece to the board
+                    file += 1
+            if file != num_files:
+                if num_files == -1:
+                    num_files = file
+                else:
+                    raise ValueError("board cannot have non-square shape")
+        if num_files > 25:
+            raise ValueError("board cannot have more than 26 files")
+        self.files: Final[int] = num_files
+        assert all([pos == piece.pos for pos,
+                   piece in self.piece_array.items()])
+        match fen_components[1]:
+            case "w":
+                self.turn: Color = Color.WHITE
+            case "b":
+                self.turn: Color = Color.BLACK
+            case _:
+                raise ValueError("current turn must be either 'w' or 'b'")
+        self.first_player: Final[Color] = self.turn
+        self.castling_rights: CastlingRights = CastlingRights.NONE
+        if "K" in fen_components[2]:
+            self.castling_rights |= CastlingRights.WHITE_KINGSIDE
+        if "Q" in fen_components[2]:
+            self.castling_rights |= CastlingRights.WHITE_QUEENSIDE
+        if "k" in fen_components[2]:
+            self.castling_rights |= CastlingRights.BLACK_KINGSIDE
+        if "q" in fen_components[2]:
+            self.castling_rights |= CastlingRights.BLACK_QUEENSIDE
+        self.en_passant: Optional[Coordinate] = (
+            None if fen_components[3] == "-" else Coordinate(fen_components[3])
+        )
+        self.halfmove_clock: int = int(fen_components[4])
+        self.fullmove_clock: int = int(fen_components[5])
+        self.ex_attributes: dict[str, Any] = ex_attributes
---
+++ pieces.py
@@ -0,0 +1,208 @@
+from typing import Final, Optional, Sequence, SupportsIndex
+
+import board
+
+# These values are placeholders that should be defined in the variant structure once it is created.
+# !!! DO NOT LEAVE THESE HERE !!!
+WHITE_PAWN_RANK: Final[int] = 1
+BLACK_PAWN_RANK: Final[int] = 6
+
+
+class Piece:
+    def __init__(
+        self, position: board.Coordinate, color: board.Color, board_ref: board.Board
+    ):
+        if not isinstance(position, board.Coordinate):
+            raise TypeError(
+                "position must be of type Coordinate (not "
+                + type(position).__name__
+                + ")"
+            )
+        elif not isinstance(color, board.Color):
+            raise TypeError(
+                "color must be of type Color (not " +
+                type(color).__name__ + ")"
+            )
+        elif not isinstance(board_ref, board.Board):
+            raise TypeError(
+                "board_ref must be of type Board (not " +
+                type(board_ref).__name__ + ")"
+            )
+        self.pos: board.Coordinate = position
+        self.color: Final[board.Color] = color
+        self.board: board.Board = board_ref
+
+    def move(self, dest: board.Coordinate):
+        "Moves the piece to the given destination, and returns the piece that was captured, if any."
+        self.board.en_passant = None
+        self.board.halfmove_clock += 1
+        self.board.turn = self.board.turn.next()
+        self.board.fullmove_clock += self.board.turn == self.board.first_player
+        try:
+            captured_piece: Optional[Piece] = self.board.piece_array[dest]
+        except KeyError:
+            captured_piece = None
+        del self.board.piece_array[self.pos]
+        self.pos = dest
+        self.board.piece_array[dest] = self
+        return captured_piece
+
+    def moves(self) -> frozenset[board.Coordinate]:
+        "Returns the set of all spaces that the piece can move to, excluding rules about check or self-capture."
+        return frozenset()
+
+
+def leap(piece: Piece, step: Sequence[SupportsIndex]) -> frozenset[board.Coordinate]:
+    "Returns the location of a leap from the piece's position with an offset given if it is a legal position."
+    if not isinstance(piece, Piece):
+        raise TypeError(
+            "piece must be of type Piece or of a type derived from Piece (not "
+            + type(piece).__name__
+            + ")"
+        )
+    try:
+        test_pos: board.Coordinate = piece.pos + step
+        if test_pos[0] >= piece.board.files or test_pos[1] >= piece.board.ranks:
+            raise IndexError
+        return frozenset({test_pos})
+    except IndexError:
+        return frozenset()
+
+
+def ride(piece: Piece, step: Sequence[SupportsIndex]) -> frozenset[board.Coordinate]:
+    "Returns a line of empty spaces from the piece's position to the next occupied square or the edge of the board, using the step size given."
+    if not isinstance(piece, Piece):
+        raise TypeError(
+            "piece must be of type Piece or of a type derived from Piece (not "
+            + type(piece).__name__
+            + ")"
+        )
+    test_pos: board.Coordinate = piece.pos
+    positions: set[board.Coordinate] = set()
+    while True:
+        try:
+            test_pos += step
+            if test_pos[0] >= piece.board.files or test_pos[1] >= piece.board.ranks:
+                raise IndexError
+        except IndexError:
+            return frozenset(positions)
+        positions.add(test_pos)
+        if test_pos in piece.board.piece_array:
+            return frozenset(positions)
+
+
+def sym_leap(
+    piece: Piece, step: Sequence[SupportsIndex]
+) -> frozenset[board.Coordinate]:
+    "Similar to the standard leap() function, but includes all possible reflections and rotations of the step value."
+    return (
+        leap(piece, step)
+        | leap(piece, (-step[0], step[1]))
+        | leap(piece, (step[0], -step[1]))
+        | leap(piece, (-step[0], -step[1]))
+        | leap(piece, (step[1], step[0]))
+        | leap(piece, (-step[1], step[0]))
+        | leap(piece, (step[1], -step[0]))
+        | leap(piece, (-step[1], -step[0]))
+    )
+
+
+def sym_ride(
+    piece: Piece, step: Sequence[SupportsIndex]
+) -> frozenset[board.Coordinate]:
+    "Similar to the standard ride() function, but includes all possible reflections and rotations of the step value."
+    return (
+        ride(piece, step)
+        | ride(piece, (-step[0], step[1]))
+        | ride(piece, (step[0], -step[1]))
+        | ride(piece, (-step[0], -step[1]))
+        | ride(piece, (step[1], step[0]))
+        | ride(piece, (-step[1], step[0]))
+        | ride(piece, (step[1], -step[0]))
+        | ride(piece, (-step[1], -step[0]))
+    )
+
+
+class Bishop(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        return sym_ride(self, (1, 1))
+
+
+class King(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        return sym_leap(self, (1, 0)) | sym_leap(self, (1, 1))
+
+
+class Knight(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        return sym_leap(self, (2, 1))
+
+
+class Pawn(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        legal_moves: set[board.Coordinate] = set()
+        test_pos: board.Coordinate
+        match self.color:
+            case board.Color.WHITE:
+                try:
+                    test_pos = self.pos + (-1, 1)
+                    if test_pos[0] >= self.board.files or test_pos[1] >= self.board.ranks:
+                        raise IndexError
+                except IndexError:
+                    pass
+                else:
+                    if test_pos == self.board.en_passant or test_pos in self.board.piece_array:
+                        legal_moves.add(test_pos)
+                try:
+                    test_pos = self.pos + (1, 1)
+                    if test_pos[0] >= self.board.files or test_pos[1] >= self.board.ranks:
+                        raise IndexError
+                except IndexError:
+                    pass
+                else:
+                    if test_pos == self.board.en_passant or test_pos in self.board.piece_array:
+                        legal_moves.add(test_pos)
+                test_pos = self.pos + (0, 1)
+                if test_pos not in self.board.piece_array:
+                    legal_moves.add(test_pos)
+                    if self.pos.pos[1] == WHITE_PAWN_RANK:
+                        test_pos += (0, 1)
+                        if test_pos not in self.board.piece_array:
+                            legal_moves.add(test_pos)
+            case board.Color.BLACK:
+                try:
+                    test_pos = self.pos + (-1, -1)
+                    if test_pos[0] >= self.board.files or test_pos[1] >= self.board.ranks:
+                        raise IndexError
+                except IndexError:
+                    pass
+                else:
+                    if test_pos == self.board.en_passant or test_pos in self.board.piece_array:
+                        legal_moves.add(test_pos)
+                try:
+                    test_pos = self.pos + (1, -1)
+                    if test_pos[0] >= self.board.files or test_pos[1] >= self.board.ranks:
+                        raise IndexError
+                except IndexError:
+                    pass
+                else:
+                    if test_pos == self.board.en_passant or test_pos in self.board.piece_array:
+                        legal_moves.add(test_pos)
+                test_pos = self.pos + (0, -1)
+                if test_pos not in self.board.piece_array:
+                    legal_moves.add(test_pos)
+                    if self.pos.pos[1] == BLACK_PAWN_RANK:
+                        test_pos += (0, -1)
+                        if test_pos not in self.board.piece_array:
+                            legal_moves.add(test_pos)
+        return frozenset(legal_moves)
+
+
+class Queen(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        return sym_ride(self, (1, 0)) | sym_ride(self, (1, 1))
+
+
+class Rook(Piece):
+    def moves(self) -> frozenset[board.Coordinate]:
+        return sym_ride(self, (1, 0))
